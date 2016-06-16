9c0d0257f2485fffb2a1dd90d05f189c952cc178
0ppy <0ppy.ja.dev@gmail.com>
2016-06-16 17:57:01 +0900

    fix compile process

--- bin/util/xdebug.sh
+++ bin/util/xdebug.sh
@@ -5,28 +5,28 @@ install_xdebug() {
   # Build Path: /app/.heroku/php/
   dep_url=git://github.com/xdebug/xdebug.git
   xdebug_dir=xdebug
-echo "-----> Building Xdbug..."
+  echo "-----> Building Xdbug..."
 
 ### Xdebug
-echo "[LOG] Downloading XDebug"
-git clone $dep_url -q
-if [ ! -d "$xdebug_dir" ]; then
-  echo "[ERROR] Failed to find xdebug directory $xdebug_dir"
-  exit
-fi
-cd $xdebug_dir/build
+  echo "[LOG] Downloading XDebug"
+  git clone $dep_url -q
+  if [ ! -d "$xdebug_dir" ]; then
+    echo "[ERROR] Failed to find xdebug directory $xdebug_dir"
+    exit
+  fi
+  cd $xdebug_dir/build
 
 # /app/php/bin/phpize
 # ./configure --enable-xdebug --with-php-config=$PHP_ROOT/bin/php-config
 # make
 # make install
-BUILD_DIR=$1
-ln -s $BUILD_DIR/.heroku /app/.heroku
-export PATH=/app/.heroku/php/bin:$PATH
-cd xdebug
-./configure --enable-xdebug
-make
-cd
-echo "important extension xdebug into php.ini"
-echo "extension=xdebug.so" >> /app/.heroku/php/etc/php/php.ini
+  BUILD_DIR=$1
+  ln -s $BUILD_DIR/.heroku /app/.heroku
+  export PATH=/app/.heroku/php/bin:$PATH
+  cd xdebug
+  ./configure --enable-xdebug
+  make
+  cd
+  echo "important extension xdebug into php.ini"
+  echo "extension=xdebug.so" >> /app/.heroku/php/etc/php/php.ini
 }
